a4e21606d0c257fc1eff6ffd0ab637ac0bd528f0
wangchuang <wangchuang@Ctrip.com>
2019-01-24 18:33:51 +0800

    dd

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-greet = "Hello Bob"
-# print(greet[0:3])
-print(greet[1:])
+# text2numbers.py
+# A program to convert a textual message into a sequence of
+# numbers, utilizing the underlying Unicode encoding.
+
+
+def main():
+    message = "abcabc"
+    print('key:', message.capitalize())     #//
+    print('key:', message.center(4))
+    print('key:', message.center(5))
+    print('key:', message.count("ab"))
+
+
+
+main()
