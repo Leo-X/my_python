9f29f0085de3c74f1240d714d2a99def1546bb3a
wangchuang <wangchuang@Ctrip.com>
2019-01-29 19:20:09 +0800

    mdf  design

--- test.py
+++ test.py
@@ -1,6 +1,33 @@
+import math
+import random
+
+
 def main():
-    i = 0
-    while i <= 10:
-        print(i)
-        i = i + 1
+    def simOneGame(pa, pb):
+        return (pa*random.uniform(0, 100), pb*random.uniform(0, 100))
+
+    def simNGames(probA, probB):
+        winsA = 0
+        winsB = 0
+        while (winsA < 15 and winsB < 15):
+            scoreA, scoreB = simOneGame(probA, probB)
+            if scoreA > scoreB:
+                winsA = winsA + 1
+            else:
+                winsB = winsB + 1
+        return (winsA, winsB)
+
+    def aWin(pa, pb):
+        a = 0
+        b = 1
+        n = 0
+        while a < b:
+            n += 1
+            a, b = simNGames(pa, pb)
+            print('a:b=', str(a)+":"+str(b))
+        print('n场比赛:', n)
+        return n
+    aWin(0.4, 0.6)
+
+
 main()
