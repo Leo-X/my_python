88152ba261175b0c2d460ee5fefabb058c02e9b0
wangchuang <wangchuang@Ctrip.com>
2019-02-01 18:26:04 +0800

    mdf  test

--- test.py
+++ test.py
@@ -4,8 +4,12 @@ import math
 
 
 def main():
-    print(round(4.51))
+    t1 = (50)
+    print('t1:', type(t1) )
+        # 不加逗号，类型为整型
 
+    t2 = ((1, 2, 4) + (4, 5, 6))
+    print('t2:', t2 )     # 加上逗号，类型为元组
 
 
 
